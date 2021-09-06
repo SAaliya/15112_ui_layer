9b4529b11d7119d1ebadfe3883352b13aac73577
Namra Attar <36705363+SAaliya@users.noreply.github.com>
2021-09-06 17:47:01 +0530

    Add files via upload

---
+++ demo02/js/main.js
@@ -0,0 +1,17 @@
+let counter=0;
+const result=document.getElementById("result");
+
+function updateResult(){
+    result.innerHTML=counter;
+
+}
+
+function increment(){
+    counter=counter+1;
+    updateResult();
+}
+
+function decrement(){
+    counter=counter-1;
+    updateResult();
+}
